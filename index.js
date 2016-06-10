2f526fc865e7eef2fc7771732c46193a21d57f3b
UberBooster <info@uberbooster.com>
2016-06-09 20:31:45 -0400

    created about and contact pages

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
 });
 
+server.get('/about', function(request, response){
+  response.sendFile('public/html/about.html', {root: __dirname});
+});
+
+server.get('/contact', function(request, response){
+  response.sendFile('public/html/contact.html', {root: __dirname});
+});
+
 server.listen(8080);
